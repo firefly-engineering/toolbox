462900f5592728a9857470047d78d5c7b0b1ffa4
Yann Hodique <yann@firefly.engineering>
2026-04-17 11:49:46 +0200

    feat: build delta from source on x86_64-darwin
    No prebuilt binary available for this platform. The builder
    auto-detects: prebuilt when a platform hash exists in data.json,
    source build otherwise.

--- packages/delta/default.nix
+++ packages/delta/default.nix
@@ -3,58 +3,89 @@
 let
   inherit (toolboxLib.readData ./data.json) meta versions;
 
+  system = pkgs.stdenv.hostPlatform.system;
+
   targetTriple = {
     "x86_64-linux" = "x86_64-unknown-linux-gnu";
     "aarch64-linux" = "aarch64-unknown-linux-gnu";
     "aarch64-darwin" = "aarch64-apple-darwin";
-  }.${pkgs.stdenv.hostPlatform.system}
-    or (throw "Unsupported system: ${pkgs.stdenv.hostPlatform.system}");
+  };
+
+  mkMeta = {
+    description = "A syntax-highlighting pager for git, diff, and grep output";
+    homepage = "https://github.com/dandavison/delta";
+    license = lib.licenses.mit;
+    mainProgram = "delta";
+  };
+
+  # Prebuilt binary for platforms with releases
+  mkPrebuilt = version: versionData:
+    let
+      triple = targetTriple.${system};
+      platformData = versionData.${system};
+    in
+    pkgs.stdenv.mkDerivation {
+      pname = "delta";
+      inherit version;
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/dandavison/delta/releases/download/${version}/delta-${version}-${triple}.tar.gz";
+        hash = platformData.sha256;
+      };
+
+      sourceRoot = "delta-${version}-${triple}";
+
+      dontConfigure = true;
+      dontBuild = true;
+      dontStrip = true;
+
+      nativeBuildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+        pkgs.autoPatchelfHook
+      ];
+
+      buildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+        pkgs.stdenv.cc.cc.lib
+      ];
+
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/bin
+        install -m755 delta $out/bin/delta
+        runHook postInstall
+      '';
+
+      meta = mkMeta;
+    };
+
+  # Build from source for platforms without prebuilt binaries
+  mkFromSource = version: versionData:
+    let
+      rust = toolbox.rust.versions.${versionData.rust};
+      rustPlatform = pkgs.makeRustPlatform { rustc = rust; cargo = rust; };
+    in
+    rustPlatform.buildRustPackage {
+      pname = "delta";
+      inherit version;
+
+      src = pkgs.fetchFromGitHub {
+        owner = "dandavison";
+        repo = "delta";
+        rev = version;
+        hash = versionData.srcHash;
+      };
+
+      cargoHash = versionData.cargoHash;
+      doCheck = false;
+
+      meta = mkMeta;
+    };
 
   builders = {
     default = version: versionData:
-      let
-        system = pkgs.stdenv.hostPlatform.system;
-        platformData = versionData.${system}
-          or (throw "delta ${version} has no binary for ${system}");
-      in
-      pkgs.stdenv.mkDerivation {
-        pname = "delta";
-        inherit version;
-
-        src = pkgs.fetchurl {
-          url = "https://github.com/dandavison/delta/releases/download/${version}/delta-${version}-${targetTriple}.tar.gz";
-          hash = platformData.sha256;
-        };
-
-        sourceRoot = "delta-${version}-${targetTriple}";
-
-        dontConfigure = true;
-        dontBuild = true;
-        dontStrip = true;
-
-        nativeBuildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-          pkgs.autoPatchelfHook
-        ];
-
-        buildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-          pkgs.stdenv.cc.cc.lib
-        ];
-
-        installPhase = ''
-          runHook preInstall
-          mkdir -p $out/bin
-          install -m755 delta $out/bin/delta
-          runHook postInstall
-        '';
-
-        meta = with lib; {
-          description = "A syntax-highlighting pager for git, diff, and grep output";
-          homepage = "https://github.com/dandavison/delta";
-          license = licenses.mit;
-          mainProgram = "delta";
-          platforms = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
-        };
-      };
+      if versionData ? ${system} then
+        mkPrebuilt version versionData
+      else
+        mkFromSource version versionData;
   };
 in
 {
