1223e46ee6744735d52d00b8f6c0b427838ba403
Yann Hodique <yann@firefly.engineering>
2026-04-27 18:10:15 +0200

    feat: add typescript 6.0.3

---
+++ packages/typescript/default.nix
@@ -0,0 +1,56 @@
+{ pkgs, lib, toolbox, toolboxLib }:
+
+let
+  inherit (toolboxLib.readData ./data.json) meta versions;
+
+  builders = {
+    default = version: versionData:
+      let
+        nodejs = toolbox.nodejs.versions.${toolbox.nodejs.default};
+      in
+      pkgs.stdenv.mkDerivation {
+        pname = "typescript";
+        inherit version;
+
+        src = pkgs.fetchurl {
+          url = "https://registry.npmjs.org/typescript/-/typescript-${version}.tgz";
+          hash = versionData.sha256;
+        };
+
+        dontConfigure = true;
+        dontBuild = true;
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/lib/node_modules/typescript $out/bin
+          cp -r . $out/lib/node_modules/typescript/
+
+          cat > $out/bin/tsc <<'WRAPPER'
+          #!/bin/sh
+          exec ${nodejs}/bin/node "$(dirname "$0")/../lib/node_modules/typescript/bin/tsc" "$@"
+          WRAPPER
+          chmod +x $out/bin/tsc
+
+          cat > $out/bin/tsserver <<'WRAPPER'
+          #!/bin/sh
+          exec ${nodejs}/bin/node "$(dirname "$0")/../lib/node_modules/typescript/bin/tsserver" "$@"
+          WRAPPER
+          chmod +x $out/bin/tsserver
+
+          runHook postInstall
+        '';
+
+        meta = with lib; {
+          description = "TypeScript language compiler and tools";
+          homepage = "https://www.typescriptlang.org";
+          license = licenses.asl20;
+          mainProgram = "tsc";
+        };
+      };
+  };
+in
+{
+  versions = toolboxLib.buildVersions "typescript" builders versions;
+  default = meta.default;
+}
