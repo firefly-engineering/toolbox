b661cec42e6a4c34e89e3bb4bc27383e3bf54445
Yann Hodique <yann@firefly.engineering>
2026-06-06 09:29:56 +0200

    feat: add qmk 1.2.0 (tb-yix.1)

---
+++ packages/qmk/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, lib, toolbox, toolboxLib }:
+
+let
+  inherit (toolboxLib.readData ./data.json) meta versions;
+
+  builders = {
+    default = version: versionData:
+      pkgs.qmk.overridePythonAttrs (old: {
+        inherit version;
+        src = pkgs.fetchPypi {
+          pname = "qmk";
+          inherit version;
+          hash = versionData.sha256;
+        };
+        propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [
+          pkgs.dos2unix
+        ];
+      });
+  };
+in
+{
+  versions = toolboxLib.buildVersions "qmk" builders versions;
+  default = meta.default;
+}
