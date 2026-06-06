b5e54983176119f1b2a48576667f70c71d92f94d
Yann Hodique <yann@firefly.engineering>
2026-06-06 09:37:27 +0200

    feat: add qmk_hid 0.1.13 (tb-ay7.1)

---
+++ packages/qmk_hid/default.nix
@@ -0,0 +1,47 @@
+{ pkgs, lib, toolbox, toolboxLib }:
+
+let
+  inherit (toolboxLib.readData ./data.json) meta versions;
+
+  builders = {
+    default = version: versionData:
+      let
+        rust = toolbox.rust.versions.${versionData.rust};
+        rustPlatform = pkgs.makeRustPlatform { rustc = rust; cargo = rust; };
+      in
+      rustPlatform.buildRustPackage {
+        pname = "qmk_hid";
+        inherit version;
+        src = pkgs.fetchFromGitHub {
+          owner = "FrameworkComputer";
+          repo = "qmk_hid";
+          rev = "v${version}";
+          hash = versionData.sha256;
+        };
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+          pkgs.pkg-config
+        ];
+
+        buildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+          pkgs.udev
+        ] ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+          pkgs.apple-sdk_15
+        ];
+
+        doCheck = false;
+
+        meta = with lib; {
+          description = "Commandline tool to interact with QMK devices via their raw HID interface";
+          homepage = "https://github.com/FrameworkComputer/qmk_hid";
+          license = licenses.bsd3;
+          mainProgram = "qmk_hid";
+        };
+      };
+  };
+in
+{
+  versions = toolboxLib.buildVersions "qmk_hid" builders versions;
+  default = meta.default;
+}
