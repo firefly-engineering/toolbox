e562b321707e9220b914b6b439527773b3c18da7
Yann Hodique <yann@firefly.engineering>
2026-06-20 16:11:53 +0200

    feat: add hunk 0.16.0 (terminal diff viewer)

---
+++ packages/hunk/default.nix
@@ -0,0 +1,65 @@
+{ pkgs, lib, toolbox, toolboxLib }:
+
+let
+  inherit (toolboxLib.readData ./data.json) meta versions;
+
+  assetPlatform = {
+    "x86_64-linux" = "linux-x64";
+    "aarch64-linux" = "linux-arm64";
+    "x86_64-darwin" = "darwin-x64";
+    "aarch64-darwin" = "darwin-arm64";
+  }.${pkgs.stdenv.hostPlatform.system}
+    or (throw "Unsupported system: ${pkgs.stdenv.hostPlatform.system}");
+
+  builders = {
+    default = version: versionData:
+      let
+        system = pkgs.stdenv.hostPlatform.system;
+        platformData = versionData.${system}
+          or (throw "hunk ${version} has no binary for ${system}");
+      in
+      pkgs.stdenv.mkDerivation {
+        pname = "hunk";
+        inherit version;
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/modem-dev/hunk/releases/download/v${version}/hunkdiff-${assetPlatform}.tar.gz";
+          hash = platformData.sha256;
+        };
+
+        sourceRoot = "hunkdiff-${assetPlatform}";
+
+        dontConfigure = true;
+        dontBuild = true;
+        dontStrip = true;
+
+        nativeBuildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+          pkgs.autoPatchelfHook
+        ];
+
+        buildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+          pkgs.stdenv.cc.cc.lib
+        ];
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/bin
+          install -m755 hunk $out/bin/hunk
+          ln -s hunk $out/bin/hunkdiff
+          runHook postInstall
+        '';
+
+        meta = with lib; {
+          description = "Review-first terminal diff viewer for agentic coders";
+          homepage = "https://github.com/modem-dev/hunk";
+          license = licenses.mit;
+          mainProgram = "hunk";
+          platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+        };
+      };
+  };
+in
+{
+  versions = toolboxLib.buildVersions "hunk" builders versions;
+  default = meta.default;
+}
